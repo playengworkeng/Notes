11e80e39fe65e9a2fffb019544bc8c15baabcea1
J <jmac@js-macbook-pro.home>
2016-08-11 21:16:05 -0400

    Initial Commit

---
+++ Notes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Notes
+//
+//  Created by J on 8/11/16.
+//  Copyright © 2016 Thinkful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Notes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Notes
+//
+//  Created by J on 8/11/16.
+//  Copyright © 2016 Thinkful. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
